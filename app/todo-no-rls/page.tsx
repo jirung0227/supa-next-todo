96886674366fd402d3de6a7ad5764c4d5925b383
정지형 <gw03176370@gmail.com>
2024-07-24 08:58:37 +0900

    feat : AppRouter 기본 설정

---
+++ app/todo-no-rls/page.tsx
@@ -0,0 +1,16 @@
+import { sleep } from "@/lib/utils";
+import React from "react";
+
+/**
+ *
+ * @description 서버 컴포넌트 : 서버에서만 동작하고 있는 리액트 컴포넌트
+ */
+const Page = async () => {
+  //   throw new Error("custom error");
+  console.log(">>API call start");
+  await sleep(1500);
+  console.log(">>API call end");
+  return <div>Page</div>;
+};
+
+export default Page;
