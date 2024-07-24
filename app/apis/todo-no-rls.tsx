f54eb764158fa30c9b67ac50297944cab9673421
정지형 <gw03176370@gmail.com>
2024-07-25 08:18:35 +0900

    feat : API 추가 - createTodos, updateTodos, deleteTodosSoft

--- app/apis/todo-no-rls.tsx
+++ app/apis/todo-no-rls.tsx
@@ -36,3 +36,49 @@ export const getTodosBySearch = async (terms: string) => {
 
   return result.data;
 };
+
+//개발모드에서는 두번 실행됨
+//todoList 생성하기
+export const createTodos = async (content: string) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .insert([{ content }])
+    .select();
+
+  return result.data;
+};
+
+export const updateTodos = async (id: number, content: string) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .update({ content, updated_at: new Date().toISOString() })
+    .eq("id", id)
+    .select();
+
+  return result.data;
+};
+
+// todoList softDelete
+export const deleteTodosSoft = async (id: number) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .update({
+      deleted_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    })
+    .eq("id", id)
+    .select();
+
+  return result.data;
+};
+
+// todoList hardDelete (실제 데이터를 삭제하는 것은 위험하므로 주석처리)
+// export const deleteTodosHard = async (id: number) => {
+//   const supabase = createSupabaseBrowserClient();
+//   const result = await supabase.from("todos_no_rls").delete().eq("id", id);
+
+//   return result.data;
+// };
