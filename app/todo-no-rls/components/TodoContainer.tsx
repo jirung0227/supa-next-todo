4ef49a4baaef2a5a4c171eec665ae16f98c50837
정지형 <gw03176370@gmail.com>
2024-07-25 08:45:54 +0900

    feat : todoController 추가 - get

--- app/todo-no-rls/components/TodoContainer.tsx
+++ app/todo-no-rls/components/TodoContainer.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { getTodos } from "@/app/apis/todo-no-rls";
 import React, { useEffect } from "react";
+import useTodosController from "../hooks/useTodosController";
 
 const TodoContainer = () => {
-  useEffect(() => {
-    getTodos();
-  }, []);
+  const { loading, todos } = useTodosController();
+  console.log("loading>>>", loading);
+  console.log("todos>>>", todos);
+
   return <div>TodoContainer</div>;
 };
 
