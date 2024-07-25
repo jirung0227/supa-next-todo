d7d215b109bbc7c0de4a0e0824e559d96e6d7e21
정지형 <gw03176370@gmail.com>
2024-07-25 12:18:18 +0900

    feat : todoController 추가 - create, update, delete

--- app/todo-no-rls/hooks/useTodosController.ts
+++ app/todo-no-rls/hooks/useTodosController.ts
@@ -1,4 +1,10 @@
-import { getTodoById, getTodos } from "@/app/apis/todo-no-rls";
+import {
+  createTodos,
+  deleteTodosSoft,
+  getTodos,
+  getTodosBySearch,
+  updateTodos,
+} from "@/app/apis/todo-no-rls";
 import { Database } from "@/types/supabase";
 import { useEffect, useState } from "react";
 
@@ -24,7 +30,42 @@ const useTodosController = () => {
     onGetTodos();
   }, []);
 
-  return { loading, todos };
+  // 비어있는 todo 생성
+  const onCreateEmptyTodos = async () => {
+    await createTodos("");
+    await onGetTodos();
+  };
+
+  // todo 업데이트
+  const onUpdateTodos = async (id: number, content: string) => {
+    await updateTodos(id, content);
+    await onGetTodos();
+  };
+
+  // todo 삭제
+  const onDeleteTodos = async (id: number) => {
+    await deleteTodosSoft(id);
+    await onGetTodos();
+  };
+
+  // todo 검색
+  const onSearchTodos = async (terms: string) => {
+    if (terms) {
+      const todoResult = await getTodosBySearch(terms);
+      if (todoResult) setTodos(todoResult);
+    } else {
+      await onGetTodos();
+    }
+  };
+
+  return {
+    loading,
+    todos,
+    onCreateEmptyTodos,
+    onUpdateTodos,
+    onDeleteTodos,
+    onSearchTodos,
+  };
 };
 
 export default useTodosController;
